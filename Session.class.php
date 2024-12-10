f0869d20ccb87208f2dc36e8d90f9c59bc3bc3e2
Arunan-Sellapandiyan <143475479+Arunan-Sellapandiyan@users.noreply.github.com>
2024-12-10 18:50:58 +0530

    COmments

---
+++ Session.class.php
@@ -0,0 +1,31 @@
+<?php
+class Session{
+     
+        public static function start(){
+            session_start();
+        }
+        public static function destroy(){
+            session_destroy();
+        }
+        public static function unset(){
+            session_unset();
+        }
+        public static function reset(){
+            session_reset();
+        }
+        public static function set($key,$value){
+            $_SESSION[$key] = $value;
+        }
+        public static function isset($key){
+            return isset($_SESSION[$key]);
+        }
+        public static function get($key,$default = false){
+            if(Session::isset($key)){
+                return $_SESSION[$key];
+            }
+            else{
+                return $default;
+            }
+        }
+}
+?>
